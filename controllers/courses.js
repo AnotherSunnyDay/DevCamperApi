fb73dddd1234f9a4321352c9c9be15163e9e2f39
Grant <grant@rsi.co>
2021-03-08 17:56:45 -0500

    implimented cascading delete for bootcamps

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,6 +1,6 @@
-const Course = require('../models/Course');
 const asyncHandler = require('../middleware/async');
 const ErrorResponse = require('../utils/errorResponse');
+const Course = require('../models/Course');
 
 // @desc    Get all courses
 // @route   GET /api/v1/courses
@@ -12,7 +12,10 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
   if (req.params.bootcampId) {
     query = Course.find({ bootcamp: req.params.bootcampId });
   } else {
-    query = Course.find();
+    query = Course.find().populate({
+      path: 'bootcamp',
+      select: 'name description',
+    });
   }
 
   const courses = await query;
