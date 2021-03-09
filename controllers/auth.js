27b32e6981b3b2861655cf535cde6457f6925a87
Grant <grant@rsi.co>
2021-03-09 14:01:08 -0500

    added protective middleware for routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,13 +16,7 @@ exports.registerUser = asyncHandler(async (req, res, next) => {
     role,
   });
 
-  // Create Token
-  const token = user.getSignedJwtToken();
-
-  res.status(200).json({
-    success: true,
-    token,
-  });
+  sendTokenResponse(user, 200, res);
 });
 
 // @desc    Login user
@@ -50,11 +44,39 @@ exports.login = asyncHandler(async (req, res, next) => {
     return next(new ErrorResponse(`Invalid credentials`, 401));
   }
 
-  // Create Token
-  const token = user.getSignedJwtToken();
+  sendTokenResponse(user, 200, res);
+});
+
+// @desc    Get Current logged in user
+// @route   POST /api/v1/auth/me
+// @access  Private
+exports.getMe = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
 
   res.status(200).json({
     success: true,
-    token,
+    user,
   });
 });
+
+// Get token from model and create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
+  // Create Token
+  const token = user.getSignedJwtToken();
+
+  const options = {
+    exipres: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') {
+    options.secure = true;
+  }
+
+  res.status(statusCode).cookie('token', token, options).json({
+    success: true,
+    token,
+  });
+};
