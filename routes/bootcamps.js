47c26f605ac30ce21e5c1f2d0e88261bbc113d1e
Grant <grant@rsi.co>
2021-03-08 21:46:33 -0500

    advancedResults middleware to include access to filtering and pagination on all routes

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -8,6 +8,8 @@ const {
   getBootcampsInRadius,
   bootcampPhotoUpload,
 } = require('../controllers/bootcamps');
+const Bootcamp = require('../models/Bootcamp');
+const advancedResults = require('../middleware/advancedResults');
 
 //Include other resource routers
 const courseRouter = require('./courses');
@@ -17,7 +19,10 @@ const router = express.Router();
 //Re-route into other recourse routers
 router.use('/:bootcampId/courses', courseRouter);
 
-router.route('/').get(getBootcamps).post(createBootcamp);
+router
+  .route('/')
+  .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
+  .post(createBootcamp);
 router
   .route('/:id')
   .get(getBootcamp)
