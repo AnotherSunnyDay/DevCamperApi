44ba039946cedc0f7eb29b9aa60633e76d1a4fa5
Grant <grant@rsi.co>
2021-03-10 12:04:13 -0500

    added reviews and implemented get and add review routes

---
+++ routes/reviews.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const { getReviews, getReview, addReview } = require('../controllers/reviews');
+const Review = require('../models/Review');
+
+const router = express.Router({ mergeParams: true });
+
+const { protect, authorize } = require('../middleware/auth');
+const advancedResults = require('../middleware/advancedResults');
+
+router
+  .route('/')
+  .get(
+    advancedResults(Review, {
+      path: 'bootcamp',
+      select: 'name description',
+    }),
+    getReviews
+  )
+  .post(protect, authorize('user', 'admin'), addReview);
+router.route('/:id').get(getReview);
+//   .put(protect, authorize('publisher', 'admin'), updateReviews)
+//   .delete(protect, authorize('publisher', 'admin'), deleteReviews);
+
+module.exports = router;
